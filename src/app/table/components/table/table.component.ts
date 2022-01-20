334ee30c54b9d48dbc7538df9fea3afee9cd81f4
Delyan Ninov <delyan.ninov@visma.com>
2022-01-20 06:41:06 +0200

    table fix

--- src/app/table/components/table/table.component.ts
+++ src/app/table/components/table/table.component.ts
@@ -1,51 +1,68 @@
-import { Component, OnInit } from "@angular/core";
-import { FormService } from "src/app/form/services/form.service";
-import { ColDef } from 'ag-grid-community'; 
+import { Component, OnInit } from '@angular/core';
+import { FormService } from 'src/app/form/services/form.service';
+import { ColDef } from 'ag-grid-community';
 import { DatePipe } from '@angular/common';
 @Component({
-    selector: 'app-table',
-    templateUrl: './table.component.html',
-    styleUrls: ['./table.component.css'],
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css'],
 })
-export class TableComponent implements OnInit{
+export class TableComponent implements OnInit {
+  constructor(private formService: FormService, private datePipe: DatePipe) {}
 
-  constructor (private formService: FormService, private datePipe: DatePipe){}
   users$ = this.formService.users$;
   columnDefs: any[] = [];
+  rowData: any[] = [];
+  ngOnInit(): void {
+    this.users$.subscribe((users) => {
+      users.map((user) => {
+        let name = user.name_cyr;
+        let dates = user.workingDays.map((user) => {
+          const userArr = Object.values(user);
+          const userObj = {
+            [userArr[0]]: userArr[1],
+          };
+          return userObj;
+        });
+        let newUser = {};
+        Object.assign(newUser, { name_cyr: name });
+        dates.forEach((date) => {
+          Object.assign(newUser, date);
+        });
+        this.rowData.push(newUser);
+      });
+    });
 
-rowData = this.formService.users$;
-    ngOnInit(): void {
-      let users = []
-      this.users$.subscribe((users)=>{
-        users.map((user)=> {
-          let name = user.name_cyr
-          let dates = user.workingDays
-          let newUser = {...dates, name}
-          console.log(newUser);
-          
-          
-        })
-        
-      })
+    this.setupTable();
 
-      const d = new Date("2022-01-01");
-      const f = new Date("2022-01-05");
-      const dates = this.getDaysArray(d, f)
-      const formatedDates = dates.map((date)=> this.datePipe.transform(date, 'yyyy-MM-dd'))
-      const finalDates = formatedDates.map((date)=> {
-        return {field: date}
-      })
-      
-      console.log(formatedDates);
-      
-    this.columnDefs.push( {field: 'name_cyr'})
-    this.columnDefs = this.columnDefs.concat(finalDates);
     // console.log(this.columnDefs);
+  }
+
+  setupTable() {
+    const d = new Date('2022-01-01');
+    const f = new Date('2022-01-05');
+    const dates = this.getDaysArray(d, f);
+    const formatedDates = dates.map((date) =>
+      this.datePipe.transform(date, 'yyyy-MM-dd')
+    );
+    const finalDates = formatedDates.map((date) => {
+      return { field: date };
+    });
+
+    console.log(formatedDates);
+
+    this.columnDefs.push({ field: 'name_cyr' });
+    this.columnDefs = this.columnDefs.concat(finalDates);
+  }
+
+  getDaysArray(start: Date, end: Date) {
+    for (
+      var arr = [], dt = new Date(start);
+      dt <= end;
+      dt.setDate(dt.getDate() + 1)
+    ) {
+      arr.push(new Date(dt));
     }
-    getDaysArray (start:Date, end:Date) {
-      for(var arr=[],dt=new Date(start); dt<=end; dt.setDate(dt.getDate()+1)){
-          arr.push(new Date(dt));
-      }
-      return arr;
-  };
+    return arr;
+  }
 }
